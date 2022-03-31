058aadefa3097e5fdb35a0d391654f75de5fd970
galmegy <djkang2008@daum.net>
2022-03-31 12:06:05 +0900

    First commit

---
+++ user.js
@@ -0,0 +1,182 @@
+var user = [
+    {
+      "_id": "62451536f45a7aa698e9350c",
+      "age": 21,
+      "eyeColor": "blue",
+      "name": "Wilda Bright",
+      "gender": "female",
+      "email": "wildabright@centrexin.com",
+      "phone": "+1 (907) 405-3956"
+    },
+    {
+      "_id": "62451536879aab3cb877ea44",
+      "age": 28,
+      "eyeColor": "green",
+      "name": "Corrine Huff",
+      "gender": "female",
+      "email": "corrinehuff@centrexin.com",
+      "phone": "+1 (941) 557-2857"
+    },
+    {
+      "_id": "624515366ee7f4bbbc711532",
+      "age": 38,
+      "eyeColor": "blue",
+      "name": "Angelita Oconnor",
+      "gender": "female",
+      "email": "angelitaoconnor@centrexin.com",
+      "phone": "+1 (853) 466-2768"
+    },
+    {
+      "_id": "624515368729c37914517bee",
+      "age": 21,
+      "eyeColor": "blue",
+      "name": "Holloway Collier",
+      "gender": "male",
+      "email": "hollowaycollier@centrexin.com",
+      "phone": "+1 (894) 476-2345"
+    },
+    {
+      "_id": "6245153659844c7425968eb1",
+      "age": 24,
+      "eyeColor": "brown",
+      "name": "Staci Murphy",
+      "gender": "female",
+      "email": "stacimurphy@centrexin.com",
+      "phone": "+1 (854) 404-2772"
+    },
+    {
+      "_id": "624515363c46005c15eabe5d",
+      "age": 21,
+      "eyeColor": "blue",
+      "name": "Rosie Steele",
+      "gender": "female",
+      "email": "rosiesteele@centrexin.com",
+      "phone": "+1 (950) 430-3966"
+    },
+    {
+      "_id": "62451536e58d9fadff32530b",
+      "age": 33,
+      "eyeColor": "blue",
+      "name": "Chaney Mccray",
+      "gender": "male",
+      "email": "chaneymccray@centrexin.com",
+      "phone": "+1 (821) 489-2791"
+    },
+    {
+      "_id": "6245153674f3d78b0e3ff0b0",
+      "age": 29,
+      "eyeColor": "blue",
+      "name": "Hancock Montgomery",
+      "gender": "male",
+      "email": "hancockmontgomery@centrexin.com",
+      "phone": "+1 (913) 484-3021"
+    },
+    {
+      "_id": "62451536ce3ca6f36f495dfe",
+      "age": 34,
+      "eyeColor": "blue",
+      "name": "Gilda Charles",
+      "gender": "female",
+      "email": "gildacharles@centrexin.com",
+      "phone": "+1 (859) 508-3095"
+    },
+    {
+      "_id": "62451536c03b31e5baa2a67f",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Hollie Roy",
+      "gender": "female",
+      "email": "hollieroy@centrexin.com",
+      "phone": "+1 (815) 500-2348"
+    },
+    {
+      "_id": "6245153689337039bb71f190",
+      "age": 26,
+      "eyeColor": "green",
+      "name": "Christy Sullivan",
+      "gender": "female",
+      "email": "christysullivan@centrexin.com",
+      "phone": "+1 (998) 525-2978"
+    },
+    {
+      "_id": "624515362974993e50a78a86",
+      "age": 34,
+      "eyeColor": "blue",
+      "name": "Nicholson Potter",
+      "gender": "male",
+      "email": "nicholsonpotter@centrexin.com",
+      "phone": "+1 (927) 516-3937"
+    },
+    {
+      "_id": "6245153670f39aeb56e57385",
+      "age": 21,
+      "eyeColor": "brown",
+      "name": "Montoya Alston",
+      "gender": "male",
+      "email": "montoyaalston@centrexin.com",
+      "phone": "+1 (992) 587-3410"
+    },
+    {
+      "_id": "62451536c24f7755f9dc2a04",
+      "age": 25,
+      "eyeColor": "brown",
+      "name": "Marian Hancock",
+      "gender": "female",
+      "email": "marianhancock@centrexin.com",
+      "phone": "+1 (829) 567-3620"
+    },
+    {
+      "_id": "62451536958af3b7889ed41c",
+      "age": 34,
+      "eyeColor": "green",
+      "name": "Delacruz Carter",
+      "gender": "male",
+      "email": "delacruzcarter@centrexin.com",
+      "phone": "+1 (949) 571-3583"
+    },
+    {
+      "_id": "624515364df0a93677a77f9b",
+      "age": 20,
+      "eyeColor": "blue",
+      "name": "Mccall Bowers",
+      "gender": "male",
+      "email": "mccallbowers@centrexin.com",
+      "phone": "+1 (959) 564-3126"
+    },
+    {
+      "_id": "624515368b33eb41659784f3",
+      "age": 39,
+      "eyeColor": "green",
+      "name": "Lindsay Love",
+      "gender": "female",
+      "email": "lindsaylove@centrexin.com",
+      "phone": "+1 (952) 598-2575"
+    },
+    {
+      "_id": "624515365ff622a94c14bf94",
+      "age": 32,
+      "eyeColor": "blue",
+      "name": "Carson Bishop",
+      "gender": "male",
+      "email": "carsonbishop@centrexin.com",
+      "phone": "+1 (934) 498-2506"
+    },
+    {
+      "_id": "624515365aed9bb401e8ded1",
+      "age": 31,
+      "eyeColor": "green",
+      "name": "Coleman Martin",
+      "gender": "male",
+      "email": "colemanmartin@centrexin.com",
+      "phone": "+1 (850) 499-3724"
+    },
+    {
+      "_id": "6245153657042ec644df2234",
+      "age": 22,
+      "eyeColor": "brown",
+      "name": "Pearl Leon",
+      "gender": "female",
+      "email": "pearlleon@centrexin.com",
+      "phone": "+1 (918) 550-3182"
+    }
+  ]
